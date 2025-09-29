d14ce2e16ffe4e467e7abfedeb4f38def434db82
Eshita Srivastava <eshita.srivastava@uconn.edu>
2025-09-29 16:52:17 -0400

    parser

--- MiniLang.hs
+++ MiniLang.hs
@@ -1,5 +1,6 @@
 module MiniLangBasic where
 import Data.Char (isDigit, isAlpha, isAlphaNum, isSpace)
+import Control.Monad.Identity (Identity)
 
 ---------------------------------------------------------------
 -- 1. Creating tokens
@@ -12,7 +13,8 @@ data Token
     | TIf
     | TThen
     | TElse
-    | TOp String
+    | TPlus
+    | TMinus
     deriving (Show,Eq)
 
 ---------------------------------------------------------------
@@ -28,7 +30,7 @@ lexTokens (c:cs)
     | isDigit c =
         let (digits, rest) = span isDigit (c:cs)
         in TInt (read digits) : lexTokens rest
-    | isAlpha c = 
+    | isAlpha c =
         let (letters, rest) = span isAlphaNum (c:cs)
         in case letters of
             "if"    -> TIf          : lexTokens rest
@@ -37,8 +39,61 @@ lexTokens (c:cs)
             "False" -> TBool False  : lexTokens rest
             _       -> TVar letters : lexTokens rest
 
-    | c == '+' = TOp "+" : lexTokens cs
-    | c == '-' = TOp "-" : lexTokens cs
+    | c == '+' = TPlus : lexTokens cs
+    | c == '-' = TMinus : lexTokens cs
 
     | otherwise = error ("Unexpected char:" ++ [c])
 
+---------------------------------------------------------------
+-- 3. Creating our own Ast tree
+---------------------------------------------------------------
+data Expr
+    = EInt Int
+    | EBool Bool
+    | EVar String
+    | EIf Expr Expr Expr
+    | EPlus Expr Expr
+    | EMinus Expr Expr
+  deriving (Show, Eq)
+
+
+---------------------------------------------------------------
+-- 4. Parser
+---------------------------------------------------------------
+parse :: [Token] -> Expr
+parse [TInt n]      = EInt n
+parse [TBool b]     = EBool b
+parse [TVar x]     = EVar x
+
+-- if expressions -> if then else
+parse (TIf : cond : thenE : elseE : []) =
+    EIf (parse [cond]) (parse [thenE]) (parse [elseE])
+
+-- plus
+parse (tok1 : TPlus : tok2 : []) =
+        EMinus (parse [tok1]) (parse [tok2])
+
+-- minus
+parse [t
+ok1, TMinus, tok2]=
+    EMinus (parse [tok1]) (parse [tok2])
+
+parse toks = error ("Cannot parse:" ++ show toks)     -- show tokens
+
+
+---------------------------------------------------------------
+-- 5. Values
+---------------------------------------------------------------
+
+data Value
+    = VInt Int
+    | VBool Bool
+    deriving (show,Eq)
+
+
+---------------------------------------------------------------
+-- actual EVALUATIR FINALLY!!!
+---------------------------------------------------------------
+
+eval :: Expr -> Value
+eval (EInt n)       = VInt n
--- README.md
+++ README.md
@@ -3,4 +3,8 @@
 A small functional interpreter written in Haskell using first principles.
 It includes simple lex (lexer) functions and a parser.
 
+
+Flowchart for a working interpreter:
+[Lexical analysis] --> [Syntactic analysis] --> **** (expr) --> [Evaluation ] --> [Output ]
+
 Checkpoint-1. September 30th
