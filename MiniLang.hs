e377229c8f9e3c13b5f34b4331fff10a7e1a73f2
Eshita Srivastava <eshita.srivastava@uconn.edu>
2025-09-29 15:42:54 -0400

    tokens done hopefully correct

--- MiniLang.hs
+++ MiniLang.hs
@@ -25,3 +25,20 @@ lexTokens :: String -> [Token]
 lexTokens [] = []
 lexTokens (c:cs)
     | isSpace c = lexTokens cs -- (h:t)
+    | isDigit c =
+        let (digits, rest) = span isDigit (c:cs)
+        in TInt (read digits) : lexTokens rest
+    | isAlpha c = 
+        let (letters, rest) = span isAlphaNum (c:cs)
+        in case letters of
+            "if"    -> TIf          : lexTokens rest
+            "else"  -> TElse        : lexTokens rest
+            "True"  -> TBool True   : lexTokens rest
+            "False" -> TBool False  : lexTokens rest
+            _       -> TVar letters : lexTokens rest
+
+    | c == '+' = TOp "+" : lexTokens cs
+    | c == '-' = TOp "-" : lexTokens cs
+
+    | otherwise = error ("Unexpected char:" ++ [c])
+
